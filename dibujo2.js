04c02837a41e19dd84049543655fc8d55a52128d
Francisco Casales <jose.casales@outlook.com>
2019-01-09 11:23:16 -0600

    Dibujo mejorado con introduccion de datos de usuario

--- dibujo2.js
+++ dibujo2.js
@@ -1,13 +1,10 @@
-var d = document.getElementById("dibujo_tecnico");
-var lienzo = d.getContext("2d");
+var texto = document.getElementById("texto_lineas");
+var boton = document.getElementById("btn_aceptar");
+boton.addEventListener("click", dibujoPorClick);
 
-for (var i = 0; i <= 499; i+=10) {
-  dibujarLinea("red", 0, 0+i, 0+i, 499);
-  dibujarLinea("blue", 499, 0+i, 499-i, 499);
-  dibujarLinea("#666666", 0+i, 0, 0, 499-i);
-  dibujarLinea("green", 0+i, 0, 499, 0+i);
-}
-dibujarMarco(499);
+var d = document.getElementById("dibujo_tecnico");
+var ancho = d.width;
+var lienzo = d.getContext("2d");
 
 function dibujarLinea(color, x_inicial, y_inicial, x_final, y_final){
   lienzo.beginPath();
@@ -23,4 +20,17 @@ function dibujarMarco(tamanio){
   dibujarLinea("white", 1, 1, 1, tamanio);
   dibujarLinea("white", tamanio, 1, tamanio, tamanio);
   dibujarLinea("white", 1, tamanio, tamanio, tamanio);
+}
+
+function dibujoPorClick(){
+  //lienzo.clearRect();
+  dibujarMarco(499);
+  console.log(lienzo);
+  var numeroDeLineas = parseInt(texto.value);
+  for (var i = 0; i <= ancho-1; i+=(ancho/numeroDeLineas)) {
+    dibujarLinea("red", 0, 0+i, 0+i, 499);
+    dibujarLinea("blue", 499, 0+i, 499-i, 499);
+    dibujarLinea("#666666", 0+i, 0, 0, 499-i);
+    dibujarLinea("green", 0+i, 0, 499, 0+i);
+  }
 }
